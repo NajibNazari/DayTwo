72594a39dee91a4ede6f1c2b9f47dde2c088716a
nazar <nazar@DESKTOP-MOV89AK>
2021-11-22 22:23:05 -0500

    Bank Info

---
+++ src/projectbank/Bankstatement.java
@@ -0,0 +1,18 @@
+package projectbank;
+
+public class Bankstatement {
+
+	public static void main(String[] args) {
+		
+		
+		String name = "Bank of America";
+		
+		double salary = 100.00;
+		
+		System.out.println("Get all emeployees have more than 100K");
+		
+		
+
+	}
+
+}
